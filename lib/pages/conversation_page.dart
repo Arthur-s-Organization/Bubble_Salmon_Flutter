abf42146c364fa4443e2d4bb945579842f6f412d
Arthur Reynet <arthur.reynet@viacesi.fr>
2025-01-10 14:20:36 +0100

    feat : added navigation managment

---
+++ lib/pages/conversation_page.dart
@@ -0,0 +1,18 @@
+import 'package:bubble_salmon/widget/custom_appBar.dart';
+import 'package:flutter/material.dart';
+
+class ConversationPage extends StatefulWidget {
+  const ConversationPage({super.key});
+
+  @override
+  State<ConversationPage> createState() => _ConversationPageState();
+}
+
+class _ConversationPageState extends State<ConversationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: CustomAppBar(),
+    );
+  }
+}
